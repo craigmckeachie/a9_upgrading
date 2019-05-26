3523700cca8bebc423f0c503c3a69f8a3fd0f850
Craig McKeachie <craigdmckeachie@gmail.com>
2019-05-26 15:22:28 -0400

    complete

--- src/angularjs/app.config.js
+++ src/angularjs/app.config.js
@@ -1,18 +1,21 @@
 'use strict';
 
-angular.
-  module('phonecatApp').
-  config(['$locationProvider' ,'$routeProvider',
-    function config($locationProvider, $routeProvider) {
-      $locationProvider.hashPrefix('!');
+angular.module('phonecatApp').config([
+  '$locationProvider',
+  '$routeProvider',
+  function config($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
 
-      $routeProvider.
-        when('/phones', {
-          template: '<phone-list></phone-list>'
-        }).
-        when('/phones/:phoneId', {
-          template: '<phone-detail></phone-detail>'
-        }).
-        otherwise('/phones');
-    }
-  ]);
+    $routeProvider
+      .when('/phones', {
+        template: '<phone-list></phone-list>'
+      })
+      .when('/phones/:phoneId', {
+        template: '<phone-detail></phone-detail>'
+      })
+      .when('/widget', {
+        template: '<app-widget></app-widget>'
+      })
+      .otherwise('/phones');
+  }
+]);
